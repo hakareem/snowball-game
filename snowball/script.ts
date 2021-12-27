dc5bbfde863689673cafa6ebb8e4168d7531294b
hakareem <harithakareem72.com>
2021-12-27 20:55:12 +0000

    far

--- snowball/script.ts
+++ snowball/script.ts
@@ -1,208 +1,130 @@
 "use strict";
-
+ 
 const canvas = <HTMLCanvasElement>document.getElementById("myCanvas");
-
+const ctx = canvas.getContext("2d");
+ 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
-const ctx = canvas.getContext("2d");
-
-// const backgroundMusic = new Audio('music/music_zapsplat_christmas_funk.mp3')
-// backgroundMusic.volume = 0.2;
-// backgroundMusic.play();
-// backgroundMusic.loop = true;
-
+// Audio //
+// var backgroundMusic = new Audio("music/music_zapsplat_winter_dance.mp3");
+// function startBackgroundMusic() {
+//   backgroundMusic.play();
+//   backgroundMusic.loop = true;
+//   backgroundMusic.volume = 0.1;
+// }
+// const hitSound = new Audio(
+//   "music/julien_matthey_impact_snowball_on_cement_002.mp3"
+// );
+// function startHitSound() {
+//   hitSound.play();
+// }
+// const throwSound = new Audio(
+//   "music/zapsplat_sport_rugby_ball_throw_pass_let_go_001_67491.mp3"
+// );
+// function startThrowSound() {
+//   throwSound.play();
+// }
+// const hurtSound = new Audio("music/zapsplat_human_male_gasp_001_19848.mp3");
+// function startHurtSound() {
+//   hurtSound.play();
+// }
+ 
 let colors: string[] = [
-  "AntiqueWhite",
-  "Aqua",
-  "Aquamarine",
-  "Beige",
-  "Bisque",
-  "Black",
-  "BlanchedAlmond",
-  "Blue",
-  "BlueViolet",
-  "Brown",
-  "BurlyWood",
-  "CadetBlue",
   "Chartreuse",
-  "Chocolate",
-  "Coral",
-  "CornflowerBlue",
-  "Cornsilk",
   "Crimson",
   "Cyan",
-  "DarkBlue",
-  "DarkCyan",
   "DarkGoldenRod",
-  "DarkGray",
-  "DarkGrey",
-  "DarkGreen",
-  "DarkKhaki",
-  "DarkMagenta",
-  "DarkOliveGreen",
-  "DarkOrange",
-  "DarkOrchid",
-  "DarkRed",
-  "DarkSalmon",
-  "DarkSeaGreen",
-  "DarkSlateBlue",
-  "DarkSlateGray",
-  "DarkSlateGrey",
-  "DarkTurquoise",
-  "DarkViolet",
   "DeepPink",
-  "DeepSkyBlue",
-  "DimGray",
-  "DimGrey",
   "DodgerBlue",
-  "FireBrick",
-  "ForestGreen",
   "Fuchsia",
-  "Gainsboro",
   "Gold",
-  "GoldenRod",
-  "Gray",
-  "Grey",
-  "Green",
-  "GreenYellow",
-  "HotPink",
-  "IndianRed",
   "Indigo",
+  "Aqua",
+  "Aquamarine",
+  "BlueViolet",
   "Ivory",
-  "Khaki",
-  "Lavender",
-  "LavenderBlush",
-  "LawnGreen",
-  "LemonChiffon",
-  "LightBlue",
-  "LightCoral",
-  "LightCyan",
-  "LightGoldenRodYellow",
-  "LightGray",
-  "LightGrey",
-  "LightGreen",
-  "LightPink",
-  "LightSalmon",
-  "LightSeaGreen",
-  "LightSkyBlue",
-  "LightSlateGray",
-  "LightSlateGrey",
-  "LightSteelBlue",
-  "LightYellow",
-  "Lime",
-  "LimeGreen",
-  "Linen",
-  "Magenta",
-  "Maroon",
-  "MediumAquaMarine",
-  "MediumBlue",
-  "MediumOrchid",
-  "MediumPurple",
-  "MediumSeaGreen",
-  "MediumSlateBlue",
-  "MediumSpringGreen",
-  "MediumTurquoise",
-  "MediumVioletRed",
-  "MidnightBlue",
-  "MintCream",
-  "MistyRose",
-  "Moccasin",
 ];
-
-let numPlayers = 1;
-
+ 
+let username: string = prompt("Enter your username")!;
+ 
+let numPlayers = 4;
+let playerRadius = 60;
+ 
 for (let i = 0; i < numPlayers; i++) {
-  Game.players.push(new Player("Harith",new Vector(Math.floor(Math.random() * 400),Math.floor(Math.random() * 400)),colors[i]));
+ 
+    let img = document.createElement("img")
+    img.src = "player images/clipart3304.png"
+ 
+    Game.players.push(new Player(username, new Vector(Math.floor(Math.random() * 400), Math.floor(Math.random() * 400)), colors[i], 50, 100, img, playerRadius));
 }
-
-// when we click on a player it shoots a snowball - just for a test not for the full game
-
-// snowball.addEventListener("click");
-
+ 
+let images = [];
+images.push("obstacle images/tree_12.png");
+images.push("obstacle images/tree_17.png");
+images.push("obstacle images/tree_23.png");
+images.push("obstacle images/tree_28.png");
+images.push("obstacle images/tree_43.png");
+images.push("obstacle images/tree_52.png");
+ 
+let numObstacles = 100;
+for (let i = 0; i < numObstacles; i++) {
+  let p = new Vector(Math.floor(Math.random() * 5000), Math.floor(Math.random() * 5000));
+  let img = document.createElement("img")
+  let indexImage = Math.floor(Math.random()*images.length)
+  img.src = images[indexImage]
+ 
+  let o = new Obstacle(p, 50 + Math.random() * 50, "lightblue", img);
+  Game.obstacles.push(o);
+}
+ 
 requestAnimationFrame(Game.cycle);
-
-// canvas.addEventListener("click", shootSnowball);
-
-// function shootSnowball(e: MouseEvent) {
-//   const p = Game.players[0];
-//   // get the position of the player and when we click within 10px of the player then shoot a snowball and don't go to the position
-//   const mouseCoord: Vector = new Vector(e.clientX, e.clientY);
-
-//   if (distanceBetween(mouseCoord, p.position) <= 20) {
-//     p.snowballs.push(new Snowball(p.position, p.direction));
-//     // p.direction only becomes a reference to p.velocity this is why p.velocity sets p.dircetion's value to 0 after running
-//     // We creating a shiny new vector for p.direction so it creates a new vector with  the values of p.velocity
-//   }
-// }
-
-// canvas.addEventListener("click", getMousePosition);
-
+ 
 function hypo(adjacent: number, opposite: number) {
   return Math.sqrt(Math.pow(adjacent, 2) + Math.pow(opposite, 2));
 }
-
+ 
 function distanceBetween(a: Vector, b: Vector) {
   return hypo(b.x - a.x, b.y - a.y);
 }
-
-// function getMousePosition(event: MouseEvent) {
-//   let p = Game.players[0];
-//   p.destination.x = event.clientX;
-//   p.destination.y = event.clientY;
-
-//   let adjacent = p.destination.x - p.position.x;
-//   let opposite = p.destination.y - p.position.y;
-
-//   p.angle = -Math.atan2(-opposite, adjacent) - Math.PI / 2;
-//   let hypotenuse = hypo(adjacent, opposite);
-//   p.velocity.x = (adjacent / hypotenuse) * 5;
-//   p.velocity.y = (opposite / hypotenuse) * 5;
-
-//   p.direction = new Vector(p.velocity.x, p.velocity.y);
-// }
-
-
-//EVENT LISTENERS
-// mousedown to choose target - draw a line from player to the mouse point
-// mousemove - to track the mouse cursor
-// mouseup - when the mouse button is let go of
-
-canvas.addEventListener("mousedown", mouseDown)
-canvas.addEventListener("mouseup", mouseUp)
-canvas.addEventListener("mousemove", mouseMovement)
-
-let mouseBtnDown = false
-let isAiming = false
-
-function mouseDown (_e:MouseEvent) {
+ 
+canvas.addEventListener("mousedown", mouseDown);
+canvas.addEventListener("mouseup", mouseUp);
+canvas.addEventListener("mousemove", mouseMovement);
+ 
+let mouseBtnDown = false;
+let isAiming = false;
+ 
+function mouseDown(_e: MouseEvent) {
   const p = Game.players[0];
-  
-  if (distanceBetween(p.position, p.target) < 40){
-    isAiming = true
+  // startBackgroundMusic();
+  if (distanceBetween(p.position, p.target) < 40) {
+    isAiming = true;
+  } else {
+    p.runToPoint(p.target);
   }
-  else {
-    p.runToPoint(p.target)
-  }
-  mouseBtnDown = true
+  mouseBtnDown = true;
 }
-// ashdiaid
-///asad
-
-// DOMMatrixReadOnlyd
-
-function mouseUp(_e:MouseEvent){
+ 
+function mouseUp(_e: MouseEvent) {
   const p = Game.players[0];
-  mouseBtnDown = false
-  if(isAiming ) {
-    p.snowballs.push(new Snowball(p.position, p.target.subtract(p.position).normalise().multiply(5)));
+  mouseBtnDown = false;
+  if (isAiming) {
+    p.snowballs.push(
+      new Snowball(
+        p.position,
+        p.target.subtract(p.position).normalise().multiply(5)
+      )
+    );
+    // startThrowSound();
   }
-  isAiming = false
+  isAiming = false;
 }
-
-
-function mouseMovement (e:MouseEvent) {
+ 
+function mouseMovement(e: MouseEvent) {
   let p = Game.players[0];
-
-  p.target = new Vector(e.clientX, e.clientY)
+ 
+  p.target = new Vector(
+    e.clientX + Camera.focus.x - canvas.width / 2,
+    e.clientY + Camera.focus.y - canvas.height / 2
+  );
 }
